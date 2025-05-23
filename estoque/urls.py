1851e13d0893f36f9242265eda8e648ea17fa11b
Kalls09 <127970151+Kalls09@users.noreply.github.com>
2025-05-23 10:40:55 -0400

    versao revisada.2

--- estoque/urls.py
+++ estoque/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('add_produto/', views.add_produto, name="add_produto"),
-    path('produto/<slug:slug>', views.produto, name="produto")
+    path('listar_produtos/', views.listar_produtos, name="listar_produtos"),
+    path('produto/<slug:slug>/', views.produto, name="produto"),
 ]
-
-    
--- estoque/views.py
+++ estoque/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import ProdutoForm
 from .models import Categoria, Produto, Imagem
 from django.http import HttpResponse
@@ -8,40 +7,17 @@ from datetime import date
 from io import BytesIO
 from django.core.files.uploadedfile import InMemoryUploadedFile
 import sys
-from django.shortcuts import redirect
 from django.urls import reverse
 from django.contrib import messages
-from rolepermissions.decorators  import has_permission_decorator
+from rolepermissions.decorators import has_permission_decorator
+
 
 @has_permission_decorator('cadastrar_produtos')
 def add_produto(request):
     if request.method == "GET":
-        nome = request.GET.get('nome')
-        categoria = request.GET.get('categoria')
-        preco_min = request.GET.get('preco_min')
-        preco_max = request.GET.get('preco_max')
-        produtos = Produto.objects.all()
+        categorias = Categoria.objects.all()
+        return render(request, 'add_produto.html', {'categorias': categorias})
 
-        if nome or categoria or preco_min or preco_max:
-            
-            if not preco_min:
-                preco_min = 0
-
-            if not preco_max:
-                preco_max = 9999999
-
-            if nome:
-                produtos = produtos.filter(nome__icontains=nome)
-
-            if categoria:
-                produtos = produtos.filter(categoria=categoria)
-
-            produtos = produtos.filter(preco_venda__gte=preco_min).filter(preco_venda__lte=preco_max)
-
-
-
-        categorias = Categoria.objects.all()       
-        return render(request, 'add_produto.html', {'categorias': categorias, 'produtos': produtos})
     elif request.method == "POST":
         nome = request.POST.get('nome')
         categoria = request.POST.get('categoria')
@@ -49,14 +25,15 @@ def add_produto(request):
         preco_compra = request.POST.get('preco_compra')
         preco_venda = request.POST.get('preco_venda')
 
-        produto = Produto(nome=nome,
-                            categoria_id=categoria,
-                            quantidade=quantidade,
-                            preco_compra=preco_compra,
-                            preco_venda=preco_venda)
-        
+        produto = Produto(
+            nome=nome,
+            categoria_id=categoria,
+            quantidade=quantidade,
+            preco_compra=preco_compra,
+            preco_venda=preco_venda
+        )
         produto.save()
-        
+
         for f in request.FILES.getlist('imagens'):
             name = f'{date.today()}-{produto.id}.jpg'
 
@@ -68,24 +45,43 @@ def add_produto(request):
             output = BytesIO()
             img.save(output, format="JPEG", quality=100)
             output.seek(0)
-            img_final = InMemoryUploadedFile(output,
-                                                'ImageField',
-                                                name,
-                                                'image/jpeg',
-                                                sys.getsizeof(output),
-                                                None
+
+            img_final = InMemoryUploadedFile(
+                output,
+                'ImageField',
+                name,
+                'image/jpeg',
+                sys.getsizeof(output),
+                None
             )
 
-
-            img_dj = Imagem(imagem = img_final, produto=produto)
+            img_dj = Imagem(imagem=img_final, produto=produto)
             img_dj.save()
+
         messages.add_message(request, messages.SUCCESS, 'Produto cadastrado com sucesso')
         return redirect(reverse('add_produto'))
 
+
+def listar_produtos(request):
+    nome = request.GET.get('nome')
+    categoria = request.GET.get('categoria')
+
+    produtos = Produto.objects.all()
+
+    if nome:
+        produtos = produtos.filter(nome__icontains=nome)
+
+    if categoria:
+        produtos = produtos.filter(categoria=categoria)
+
+    categorias = Categoria.objects.all()
+
+    return render(request, 'listar_produtos.html', {
+        'produtos': produtos,
+        'categorias': categorias
+    })
+
+
 def produto(request, slug):
-    if request.method == "GET":
-        produto = Produto.objects.get(slug=slug)
-        data = produto.__dict__
-        data['categoria'] = produto.categoria.id
-        form = ProdutoForm(initial=data)
-        return render(request, 'produto.html', {'form': form})
+    produto = get_object_or_404(Produto, slug=slug)
+    return render(request, 'produto.html', {'produto': produto})
